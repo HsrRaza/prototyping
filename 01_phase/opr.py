55c0501f0cd8d9fc40e33870509b56306c63f8ff
HsrRaza <hr.raza04@gmail.com>
2026-07-05 01:17:12 +0530

    list tuples

---
+++ 01_phase/opr.py
@@ -0,0 +1,75 @@
+age  = 20
+salary = 50000
+
+if(age > 18 and salary > 40000):
+    print("Eligible for loan")
+
+
+# for i in range(0 , 10 ,2):
+    # print(i)
+
+
+# for i in range(10, 0 , -1):
+    # print(i)
+
+
+# while loop
+# i = 1
+# while(i <= 5):
+    # print(i)
+    # i +=1
+
+# break means stop loop
+
+# for i in range(10):
+#     if(i==5):
+#         break
+#     print(i)
+
+
+# continue means skip the current iteration and continue with the next iteration
+
+# for i in range(5):
+#     if(i==2):
+#         continue
+#     print(i)
+
+numbers  = [1,2,3,4,5]
+
+numbers[1] = 100
+numbers.append(6)
+numbers.remove(3)
+# print(numbers)
+# print(len(numbers))
+
+
+# for i in numbers:
+#     print(i)
+
+person = ("Hassan" , 22)
+# print(person)
+
+user = {
+    "name":"hassan",
+    "age": 22,
+
+}
+# print(user["name"])
+
+# update the value of a key in a dictionary
+user['name'] = "Hsr"
+# print(user["name"])
+
+user["city"] = "Blr"
+
+# loop for dictionary
+# for key, value  in user.items():
+    # print(key, value)
+
+# nums = {1,2,3,4,5}
+
+# if 3 in nums:
+#     print("3 is present in the set")
+
+
+# function
