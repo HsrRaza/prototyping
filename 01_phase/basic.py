3894f22344ea2325e7216574ff72be4df81519e2
HsrRaza <hr.raza04@gmail.com>
2026-07-04 22:44:17 +0530

    operators and types

---
+++ 01_phase/basic.py
@@ -0,0 +1,29 @@
+age = int(input("Enter your age: "))
+
+if(age > 20):
+    print("Adult")
+elif(age > 13):
+    print("Teenager")
+else:
+    print("child")
+
+
+
+# name = "Hsr"
+
+# print(type(age))
+# print(type(name))
+
+
+a =5
+b =2
+
+# print(a+b)
+# print(a-b)
+# print(a*b)
+# print(a/b)
+# print(a//b)
+# print(a%b)
+# print(a**b)
+
+
