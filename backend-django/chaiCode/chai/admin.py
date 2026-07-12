b9cd1a2828a33af84f0590ef8bb619ddb55da509
HsrRaza <hr.raza04@gmail.com>
2026-07-13 01:57:07 +0530

    admin panel

--- backend-django/chaiCode/chai/admin.py
+++ backend-django/chaiCode/chai/admin.py
@@ -8,9 +8,23 @@ class ChaiReviewInLine(admin.TabularInline):
     extra =2
 
 class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display= ('name' , 'type' ,'date_added')
+    inlines = [ChaiReviewInLine]
+
     
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal =  ('chai_varities' ,)
 
 
-admin.site.register(ChaiVarity)
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
+
+
+
+
+admin.site.register(ChaiVarity, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
 
 
